47730bad9c382554ec6cba003ea415f1309ceea2
NISH2AMBE <77950221+NISH2AMBE@users.noreply.github.com>
2021-10-21 21:10:50 +0530

    Add files via upload

---
+++ CALCULATION.py
@@ -0,0 +1,16 @@
+# CALCULATION
+
+a = int(input(" Enter Your First Number :-->"))
+b = int(input(" Enter Your Second Number :-->"))
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+print(f"{a} + {b} = {c}")
+print(f"{a} - {b} = {d}")
+print(f"{a} * {b} = {e}")
+print(f"{a} / {b} = {f}")
+
+
+
+
